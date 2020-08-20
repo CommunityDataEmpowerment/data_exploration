3603fd5467e25ec7308f95e05b4bc96f9f92b1f8
Richard Dunks <richard@datapolitan.com>
2020-08-19 22:43:08 -0500

    matched with 2019

--- create_uof_filename.sql
+++ create_uof_filename.sql
@@ -5,44 +5,44 @@
 
 DROP TABLE IF EXISTS cdep.uof_filenum;
 
-CREATE TABLE cdep.uof_filenum AS (
-	OBJECTID varchar,
-	FILENUM varchar,
-	Address text,
-	OCCURRED_D date,
-	OCCURRED_T time,
-	CURRENT_BA varchar,
-	OffSex text,
-	OffRace text,
-	HIRE_DT date,
-	OFF_INJURE text, --could be boolean if I trusted they were all correct
-	OffCondTyp text,
-	OFF_HOSPIT text, --could be boolean if I trusted they were all correct
-	SERVICE_TY text,
-	UOF_REASON text,
-	Cycles_Num text,
-	STREET_N varchar,
-	STREET text,
+CREATE TABLE cdep.uof_filenum (
+	objectid varchar,
+	zip varchar,
+	filenum varchar,
+	occurred_d date,
+	occurred_t time,
+	current_ba varchar,
+	offsex text,
+	offrace text,
+	hire_dt date,
+	off_injure text, --could be boolean if i trusted they were all correct
+	offcondtyp text,
+	off_hospit text, --could be boolean if i trusted they were all correct
+	service_ty text,
+	uof_reason text,
+	cycles_num text,
+	street_n varchar,
+	street text,
 	street_g text,
 	street_t text,
-	CitNum varchar,
-	CitRace text,
-	CitSex text,
-	CIT_INJURE text, --could be boolean if I trusted they were all correct
-	CitCondTyp text,
-	CIT_ARREST text,
-	CIT_INFL_A text,
-	CitChargeT text,
-	RA varchar,
-	BEAT varchar,
-	SECTOR varchar,
-	DIVISION text,
-	DIST_NAME varchar,
-	TAAG_Name text,
-	X numeric,
-	Y numeric,
-	GeoLocation json,
-	Year_reported varchar
+	address text,
+	citnum varchar,
+	citrace text,
+	citsex text,
+	cit_injure text, --could be boolean if i trusted they were all correct
+	citcondtyp text,
+	cit_arrest text,
+	cit_infl_a text,
+	citcharget text,
+	council_district text,
+	ra varchar,
+	beat varchar,
+	sector varchar,
+	division text,
+	x numeric,
+	y numeric,
+	geocoded_column json,
+	year_reported varchar
 );
 
 --FILENUMs are repeated for the same incident with multiple victims
@@ -52,6 +52,7 @@ ALTER TABLE cdep.uof_filenum ADD PRIMARY KEY (OBJECTID,FILENUM);
 
 COMMENT ON TABLE cdep.uof_filename IS 'collection of uses of force reports for Dallas Police Department from the City of Dallas Open Data Portal';
 COMMENT ON COLUMN cdep.uof_filename.OBJECTID IS 'unique object id from database';
+COMMENT ON COLUMN cdep.uof_filename.zip IS 'ZIP code of UoF incident';
 COMMENT ON COLUMN cdep.uof_filename.FILENUM IS 'UOF file number for incident. Will repeat for each victim in the incident';
 COMMENT ON COLUMN cdep.uof_filename.Address IS 'address of UoF incident';
 COMMENT ON COLUMN cdep.uof_filename.OCCURRED_D IS 'date of UoF incident';
@@ -66,9 +67,9 @@ COMMENT ON COLUMN cdep.uof_filename.OFF_HOSPIT IS 'indication of hospitalization
 COMMENT ON COLUMN cdep.uof_filename.SERVICE_TY IS 'type of policing service being rendered';
 COMMENT ON COLUMN cdep.uof_filename.UOF_REASON IS 'reason for use of force';
 COMMENT ON COLUMN cdep.uof_filename.Cycles_Num IS 'unclear what this means';
-COMMENT ON COLUMN cdep.uof_filename.STREET_N IS 'street number for incident';
+COMMENT ON COLUMN cdep.uof_filename.STREET_N IS 'street number for UoF incident';
 COMMENT ON COLUMN cdep.uof_filename.STREET IS 'street name for incident';
-COMMENT ON COLUMN cdep.uof_filename.street_g IS 'street direction for incident';
+COMMENT ON COLUMN cdep.uof_filename.street_g IS 'street direction for UoF incident';
 COMMENT ON COLUMN cdep.uof_filename.street_t IS 'street name appendix';
 COMMENT ON COLUMN cdep.uof_filename.CitNum IS 'name of victim';
 COMMENT ON COLUMN cdep.uof_filename.CitRace IS 'race of victim';
@@ -82,8 +83,8 @@ COMMENT ON COLUMN cdep.uof_filename.RA IS 'DPD Reporting Area (administrative di
 COMMENT ON COLUMN cdep.uof_filename.BEAT IS 'DPD Beat (administrative division)';
 COMMENT ON COLUMN cdep.uof_filename.SECTOR IS 'DPD Sector (administrative division)';
 COMMENT ON COLUMN cdep.uof_filename.DIVISION IS 'DPD Division (administrative division)';
-COMMENT ON COLUMN cdep.uof_filename.DIST_NAME IS 'City of Dallas Council District';
-COMMENT ON COLUMN cdep.uof_filename.TAAG_Name IS 'Targeted Action Area Grid (enhanced enforcement area)';
+COMMENT ON COLUMN cdep.uof_filename.council_district IS 'City of Dallas Council District';
 COMMENT ON COLUMN cdep.uof_filename.X IS 'x coordinate in Texas North Central State Plane EPSG:2276';
 COMMENT ON COLUMN cdep.uof_filename.Y IS 'y coordinate in Texas North Central State Plane EPSG:2276';
 COMMENT ON COLUMN cdep.uof_filename.GeoLocation IS 'json of address and latitude and longitude';
+COMMENT ON COLUMN cdep.uof_filename.Year_reported IS 'Year the UoF incident was reported';
