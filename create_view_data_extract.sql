d291aa3460e218d24f0c3af7549f4dd4b8de160e
Richard Dunks <richard@datapolitan.com>
2020-09-03 22:12:38 -0500

    revising view to include forcetype scale

--- create_view_data_extract.sql
+++ create_view_data_extract.sql
@@ -3,7 +3,7 @@
 DROP VIEW IF EXISTS cdep.data_extract;
 
 CREATE VIEW cdep.data_extract AS 
-    SELECT DISTINCT ON (f.filenum,u.uofnum,f.citnum) f.*,u.uofnum,u.forceeffec,u.forcetype, o.off_name 
+    SELECT DISTINCT ON (f.filenum,u.uofnum,f.citnum) f.*,u.uofnum,u.forceeffec,u.forcetype, o.off_name, ft.dpd_level, ft.physical_contact, ft.victim_danger 
     FROM cdep.uof_filenum AS f
     --join the uof
     LEFT OUTER JOIN cdep.uof_num as u
@@ -11,5 +11,7 @@ CREATE VIEW cdep.data_extract AS
     --join the officer information
     LEFT OUTER JOIN cdep.officer as o
         ON f.current_ba = o.badge_num
+    LEFT OUTER JOIN cdep.forcetype as ft
+        ON u.forcetype = ft.forcetype
     ORDER BY f.filenum
 ;
